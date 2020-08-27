90bc401f1bd922488e43ef95889eba594fb5ba21
Ryan L Buchanan <buchanan.ryan22@gmail.com>
2020-08-27 12:40:57 -0600

    Break

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,32 @@
-/* global document */
+/* global document, window, localStorage */
 
+// Code for heading changer
 // var myHeading = document.querySelector('h1')
 //
 // myHeading.textContent = 'Living the Dream!';
 
-document.querySelector("html").onclick = function() {
-  alert("Ouch, Stop poking me!");
-}
+// Code for image onclick event
+// document.querySelector("html").onclick = function() {
+//   alert("Ouch, Stop poking me!");
+// }
 
-document.querySelector("button").onclick = function() {
-  alert("Living the dream!!!");
-}
+// // Personalized welcome message Code
+// var nameButton = document.querySelector('button');
+// var myHeading = document.querySelector('h1');
+//
+// function setUserName() {
+//   'use strict';
+//   var myName = window.prompt('Please enter your name');
+//   localStorage.setItem('name', myName);
+//   myHeading.textContent = 'Have a lovely day, ' + myName;
+// }
+// if (!localStorage.getItem('name')) {
+//   setUserName();
+// } else {
+//   var storedName = localStorage.getItem('name');
+//   myHeading.textContent = 'Have a lovely day, ' + storedName;
+// }
+// nameButton.onclick = function() {
+//   'use strict';
+//   setUserName();
+// };
