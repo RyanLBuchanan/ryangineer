28192d511d238ac9020ec3ee55c44d9c70fd85d7
Ryan L Buchanan <buchanan.ryan22@gmail.com>
2020-08-27 09:04:51 -0600

    Click event added

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,7 @@
-var myHeading = document.querySelector('h1')
-
+// var myHeading = document.querySelector('h1')
+//
 // myHeading.textContent = 'Living the Dream!';
+
+document.querySelector("html").onclick = function() {
+  alert("Ouch, Stop poking me!");
+}
