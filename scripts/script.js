8d450ebc60b3d4465a67fac4815d9bc1ea4c79b0
Ryan L Buchanan <buchanan.ryan22@gmail.com>
2020-08-27 12:04:10 -0600

    Break

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,13 @@
+/* global document */
+
 // var myHeading = document.querySelector('h1')
 //
 // myHeading.textContent = 'Living the Dream!';
 
 document.querySelector("html").onclick = function() {
   alert("Ouch, Stop poking me!");
+}
+
+document.querySelector("button").onclick = function() {
+  alert("Living the dream!!!");
 }
