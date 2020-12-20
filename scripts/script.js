64465c6f850f02176f5c246d334ab3e0c43998c2
Ryan L Buchanan <buchanan.ryan22@gmail.com>
2020-12-19 18:42:41 -0700

    chore: Commented out dark mode js

--- scripts/script.js
+++ scripts/script.js
@@ -10,19 +10,21 @@
 //   alert("Ouch, Stop poking me!");
 // }
 
-const options = {
-  bottom: '32px', // default: '32px'
-  right: '32px', // default: '32px'
-  left: 'unset', // default: 'unset'
-  time: '0.5s', // default: '0.3s'
-  mixColor: '#fff', // default: '#fff'
-  backgroundColor: '#fff',  // default: '#fff'
-  buttonColorDark: '#100f2c',  // default: '#100f2c'
-  buttonColorLight: '#fff', // default: '#fff'
-  saveInCookies: true, // default: true,
-  label: '🌓', // default: ''
-  autoMatchOsTheme: true // default: true
-}
+
+''' Dark mode code '''
+// const options = {
+//   bottom: '32px', // default: '32px'
+//   right: '32px', // default: '32px'
+//   left: 'unset', // default: 'unset'
+//   time: '0.5s', // default: '0.3s'
+//   mixColor: '#fff', // default: '#fff'
+//   backgroundColor: '#fff',  // default: '#fff'
+//   buttonColorDark: '#100f2c',  // default: '#100f2c'
+//   buttonColorLight: '#fff', // default: '#fff'
+//   saveInCookies: true, // default: true,
+//   label: '🌓', // default: ''
+//   autoMatchOsTheme: true // default: true
+// }
 
 const darkmode = new Darkmode(options);
 darkmode.showWidget();
